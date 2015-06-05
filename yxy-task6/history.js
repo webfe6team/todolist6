613b72c7100cbec534a827c7daa82487637f6c6e
ChrisCindy <yxy0919@foxmail.com>
2015-06-05 16:53:10 +0800

    更新数据类型

--- yxy-task6/history.js
+++ yxy-task6/history.js
@@ -11,34 +11,32 @@ $(document).ready(function(){
           for(var i = 0;i<list.completedTask.length;i++){
             var taskName = list.completedTask[i].taskName,
                 taskDescribe = list.completedTask[i].taskDescribe,
-                taskType = list.completedTask[i].taskType,
-                taskMark = list.completedTask[i].taskMark,
+                priority = list.completedTask[i].priority,
                 startTime = list.completedTask[i].startTime,
-                taskState = list.completedTask[i].taskState,
                 endTime = list.completedTask[i].endTime;
-            var newCard = $(".card").clone();
-            newCard.find(".taskName") = text("任务名称："+taskName);
-            newCard.find(".taskType") = text("任务类型："+taskType);
-            newCard.find(".taskDescribe") = text("任务描述："+taskDescribe);
-            newCard.find(".startTime") = text("开始时间："+startTime);
-            newCard.find(".endTime") = text("结束时间"+endTime);
+            var newCard = $(".yxy-card").clone();
+            newCard.find(".yxy-taskName") = text("任务名称："+taskName);
+            newCard.find(".yxy-priority") = text("任务类型："+priority);
+            newCard.find(".yxy-taskDescribe") = text("任务描述："+taskDescribe);
+            newCard.find(".yxy-startTime") = text("开始时间："+startTime);
+            newCard.find(".yxy-endTime") = text("结束时间"+endTime);
             $("#yxy-completedTask").append(newCard);
           }
           //插入已删除任务
           for(var j = 0;j < list.deletedTask.length;j++){
             var taskName = list.deletedTask[i].taskName,
                 taskDescribe = list.deletedTask[i].taskDescribe,
-                taskType = list.deletedTask[i].taskType,
+                priority = list.deletedTask[i].priority,
                 taskMark = list.deletedTask[i].taskMark,
                 startTime = list.deletedTask[i].startTime,
                 taskState = list.deletedTask[i].taskState,
                 endTime = list.deletedTask[i].endTime;
             var newCard = $(".card").clone.show();
-            newCard.find(".taskName") = text("任务名称："+taskName);
-            newCard.find(".taskType") = text("任务类型："+taskType);
-            newCard.find(".taskDescribe") = text("任务描述："+taskDescribe);
-            newCard.find(".startTime") = text("开始时间："+startTime);
-            newCard.find(".endTime") = text("结束时间"+endTime);
+            newCard.find(".yxy-taskName") = text("任务名称："+taskName);
+            newCard.find(".yxy-priority") = text("任务类型："+priority);
+            newCard.find(".yxy-taskDescribe") = text("任务描述："+taskDescribe);
+            newCard.find(".yxy-startTime") = text("开始时间："+startTime);
+            newCard.find(".yxy-endTime") = text("结束时间"+endTime);
             $("#yxy-deletedTask").append(newCard);
           }
             //$("#task").html(data);
