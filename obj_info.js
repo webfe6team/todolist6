701eca4a0e44f88218ac86839f971bc93574fd98
rgy19930329 <593289488@qq.com>
2015-06-05 16:46:23 +0800

    obj_info.js 后台数据接口更改

--- obj_info.js
+++ obj_info.js
@@ -12,40 +12,50 @@
 task = {
 	"taskName" : "todolist6",
 	"taskDescribe" : "360实训todolist小组项目",
-	"taskType" : "互联网项目",
-	"taskMark" : true,
+	"priority" : "紧急",//任务优先级：普通，紧急，非常紧急
 	"startTime" : "2015-6-1",
-	"taskState" : false,
 	"endTime" : ""
 }
 
 
-信息对象：info
+项目对象：item
 
-1. 当前任务(currentTask: list)
+1. 项目名称(itemName: String)
 
-2. 已完成的任务(completedTask: list)
+2. 项目类型(itemType: String)// 互联网，媒体，……
 
-3. 已删除的任务(deletedTask: list)
+3. 项目描述(itemDescribe: String)
 
-info = {
+4. 是否为星标项目(isStar: boolen)
+
+5. 项目是否公开(isPublic: boolen)
+
+6. 项目中当前任务(currentTask: list)
+
+7. 项目中已完成的任务(completedTask: list)
+
+8. 项目中已删除的任务(deletedTask: list)
+
+item = {
+	"itemName" : "第一个项目",
+	"itemType" : "这是项目类型",
+	"itemDescribe" : "这是项目描述",
+	"isStar" : false,
+	"isPublic" : false,
+
 	"currentTask" : [
 		{
 			"taskName" : "todolist6",
 			"taskDescribe" : "360实训todolist小组项目",
-			"taskType" : "互联网任务",
-			"taskMark" : true,
+			"priority" : "紧急",
 			"startTime" : "2015-6-1",
-			"taskState" : false,
 			"endTime" : ""
 		},
 		{
 			"taskName" : "123",
 			"taskDescribe" : "P25第6题",
-			"taskType" : "其它任务",
-			"taskMark" : false,
+			"priority" : "紧急",
 			"startTime" : "2015-5-26",
-			"taskState" : false,
 			"endTime" : ""
 		}
 	],
@@ -54,10 +64,8 @@ info = {
 		{
 			"taskName" : "数学作业",
 			"taskDescribe" : "P55第6题",
-			"taskType" : "其它任务",
-			"taskMark" : true,
+			"priority" : "紧急",
 			"startTime" : "2015-6-1",
-			"taskState" : true,
 			"endTime" : "2015-6-2"
 		}
 	],
@@ -66,13 +74,85 @@ info = {
 		{
 			"taskName" : "ppt制作",
 			"taskDescribe" : "制作关于一季度市场销售状况介绍的ppt",
-			"taskType" : "媒体任务",
-			"taskMark" : true,
+			"priority" : "紧急",
 			"startTime" : "2015-6-1",
-			"taskState" : true,
 			"endTime" : "2015-6-2"
 		}
 	]
 }
+
+
+
+
+
+
+info对象：
+
+info = {
+	"itemList" : [item1, item2]
+}
+
+
+var item1 = {
+	"itemName" : "第一个项目",
+	"itemType" : "这是项目类型",
+	"itemDescribe" : "这是项目描述",
+	"isStar" : false,
+	"isPublic" : false,
+
+	"currentTask" : [
+		{
+			"taskName" : "todolist6",
+			"taskDescribe" : "360实训todolist小组项目",
+			"priority" : "紧急",
+			"startTime" : "2015-6-1",
+			"endTime" : ""
+		},
+		{
+			"taskName" : "123",
+			"taskDescribe" : "P25第6题",
+			"priority" : "紧急",
+			"startTime" : "2015-5-26",
+			"endTime" : ""
+		}
+	],
+}
+
+
+var item2 = {
+	"itemName" : "第一个项目",
+	"itemType" : "这是项目类型",
+	"itemDescribe" : "这是项目描述",
+	"isStar" : false,
+	"isPublic" : false,
+
+	"completedTask" : [
+		{
+			"taskName" : "数学作业",
+			"taskDescribe" : "P55第6题",
+			"priority" : "紧急",
+			"startTime" : "2015-6-1",
+			"endTime" : "2015-6-2"
+		}
+	],
+
+	"deletedTask" : [
+		{
+			"taskName" : "ppt制作",
+			"taskDescribe" : "制作关于一季度市场销售状况介绍的ppt",
+			"priority" : "紧急",
+			"startTime" : "2015-6-1",
+			"endTime" : "2015-6-2"
+		}
+	]
+}
+
+
+
+var info = {
+	"itemList" : [item1, item2]
+}
+
+
 
 
